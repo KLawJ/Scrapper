068ae270409853f53e35a9335937684292467f88
Athul Raj <athulbraj@gmail.com>
2017-03-31 20:47:29 +0530

    Auto

--- exec.sh
+++ exec.sh
@@ -1,6 +1,7 @@
-rm -R data
-mkdir data
-mkdir log
+$path="/home/ubuntu/Scrapper/"
+rm -R "$pathdata"
+mkdir "$pathdata"
+mkdir "$pathlog"
 host="http://klawj.tk"
 infoURL="$host/smsapi"
 echo $infoURL
@@ -15,20 +16,20 @@ h=`curl -s -o /dev/null -w "%{response_code}" $g`
 if [ $h == '200' ] && [ ! -f "log/$f" ]
 then
 
-cd data
+cd "$pathdata"
 wget $g
 pdf2txt -t text -o cl.txt $f
 wget $infoURL
 cd ..
 
-nodeOut=`node processor.js cl.txt smsapi`
+nodeOut=`node $pathprocessor.js cl.txt smsapi`
 
 if [ "$3" == 'TB' ] || [ "$2" == 'TB' ]
 then
 curl \
 -X POST \
 --form-string chat_id="@KLJICO" \
--F document="@data/smslist.json" \
+-F document="$pathdata/smslist.json" \
 https://api.telegram.org/bot373825778:AAEY4GbXCJvM09x2NICVdiu38JkwnuvoWk8/sendDocument
 fi
 
@@ -39,13 +40,14 @@ curl \
 -H "Authorization:Basic a2xqaWNvOktsSkljTw==" \
 -H "Content-Type:application/json" \
 -H "Accept:application/json" \
---data-ascii "@data/smslist.json" \
+--data-ascii "$pathdata/smslist.json" \
 https://smsapi.runacorp.com/restapi/sms/1/text/multi
 fi
 
 echo "Done."
-echo "Done" > "log/$f"
+echo "Done" > "$pathlog/$f"
 echo "Node App Output :-"
 echo $nodeOut
 fi
+
 
